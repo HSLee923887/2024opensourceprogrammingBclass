6a326cee3347a2889acd41c0074a52f300877fef
ttakzo <dlgotjd2003@gmail.com>
2024-09-27 18:24:26 +0900

    v0.2 IO & Types

---
+++ week05/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var i int
+	// i = 55
+
+	//var i int = 55
+	var f float32 = 1.92
+
+	i := 55
+	fmt.Println("F is", f)
+	fmt.Println("I is", i)
+	fmt.Print("I is ", i, "\n")
+	fmt.Println("I is", i)
+	fmt.Printf("I is %d\n", i)
+}
