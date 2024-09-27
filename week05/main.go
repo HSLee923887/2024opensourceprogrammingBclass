e73ee94037c812c5e7f5cd55eb40622e6b862227
ttakzo <dlgotjd2003@gmail.com>
2024-09-27 18:42:28 +0900

    v0.3 IO & Types

--- week05/main.go
+++ week05/main.go
@@ -1,18 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"reflect"
+	"strings"
+)
 
 func main() {
 	// var i int
 	// i = 55
 
 	//var i int = 55
-	var f float32 = 1.92
+	var f float64 = 1.92
 
-	i := 55
+	//f := 1.92
+	i := "55"
+	fmt.Println(strings.Title("kim inha"))
+	fmt.Printf("%f %f\n", f, math.Ceil(f))
+	fmt.Println(reflect.TypeOf(f), reflect.TypeOf(i))
 	fmt.Println("F is", f)
 	fmt.Println("I is", i)
 	fmt.Print("I is ", i, "\n")
 	fmt.Println("I is", i)
-	fmt.Printf("I is %d\n", i)
+	//fmt.Printf("I is %d\n", i)
+	fmt.Printf("I is %s\n", i)
 }
