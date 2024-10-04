4882cc731708f26ff207dcf42c24a3cc0a639daa
ttakzo <dlgotjd2003@gmail.com>
2024-10-04 17:55:25 +0900

    v0.5

--- week06/main.go
+++ week06/main.go
@@ -8,9 +8,14 @@ import (
 func main() {
 	i := 13
 	f := 12.9
+	c1 := 'Z'
+	c2 := '김'
 
 	fmt.Printf("value i : %d, value f : %f\n", i, f)
 	// fmt.Printf("%d*%f\n", i, f, i*f) // invalid operation: i * f (mismatched types int and float64)
 	fmt.Printf("%d*%f=%f\n", i, f, float64(i)*f) // conversion 명시적 형변환 해야함 int(f)도 가능
-	fmt.Println(reflect.TypeOf(f))
+	fmt.Println(c1, c2)                          // 10진수 출력
+	fmt.Printf("%x\n", c2)                       // 유니코드 '김'에 대한 16진수 출력, UNICODE
+	fmt.Println(reflect.TypeOf(f), reflect.TypeOf(i), reflect.TypeOf(c1), reflect.TypeOf(c2))
+
 }
